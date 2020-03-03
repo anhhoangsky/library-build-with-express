59f2d2ceec5fe72c4a6ae80891b10c0d59f4e14a
anhsky <an38570@gmail.com>
2020-03-03 22:32:26 +0700

    fix db not define

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,3 +1,7 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 module.exports.loginSession = (req, res, next)=>{
     // console.log(req.cookies)
     if(!req.cookies.username){
